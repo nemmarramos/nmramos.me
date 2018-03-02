afeb05628c9c1be0944df05df8b439751d52c229
Nemmar Ramos <neu.nmr@gmail.com>
2018-03-02 17:28:23 +0800

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    url: 'https://lumen.netlify.com',
-    title: 'Blog by John Doe',
+    url: 'https://nmramos.netlify.com',
+    title: 'Blog by Nemmar Ramos',
     subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     copyright: '© All rights reserved.',
     disqusShortname: '',
